effacfcf40cac2ee83828c91068dde905a2f11cc
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2023-05-01 23:01:57 +0530

    router updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 // import { LocalizationProvider } from "@mui/x-date-pickers";
@@ -10,8 +10,8 @@ import { Provider } from "react-redux";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
